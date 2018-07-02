7828e0f6d135e8533aef0ab4b861a75daef16644
PAbishai <phillipabishai@gmail.com>
2018-07-02 15:28:37 +0300

    Added admin function to change user roles and delete comment

--- model.py
+++ model.py
@@ -3,12 +3,16 @@ from datetime import datetime
 comment_list = []
 the_comment = {}
 user_info = []
-user_details = {}
 
 def timestamp():
     ourtime = datetime.now()
-    ourtime=datetime.strptime(ourtime, '%d%b%Y')
+    #ourtime=datetime.strptime(ourtime, '%d%b%Y')
     return ourtime
+
+def create_id(list):
+    id = len(list)+1
+    return id
+
 
 class User:
     def __init__(self,username,password):
@@ -17,14 +21,28 @@ class User:
         self.timestamp = timestamp()
 
 class Moderator(User):
-    def __init__(self):
+    def __init__(self, username, password):
         User.__init__(self,username,password)
         self.is_moderator = True
 
 class Admin(Moderator):
-    def __init__(self):
-        Moderator.__init__(self)
+    def __init__(self, username, password):
+        Moderator.__init__(self, username, password)
         self.is_admin = True
+
+    @staticmethod
+    def assign_moderator(username):
+         for user in user_info:
+            if user["name"] == username:
+                moderator = Moderator(user.username,user.password)
+                return moderator
+
+    @staticmethod
+    def assign_admin(username):
+        for user in user_info:
+            if user["name"] == username:
+                admin = Moderator(user.username,user.password)
+                return admin
 
 
 class Comment:
@@ -33,9 +51,20 @@ class Comment:
         self.timestamp = timestamp()
         self.ID = len(comment_list) + 1
 
+    @staticmethod
+    def delete_comment(id):
+        for comment in comment_list:
+            if comment["id"] == id:
+                comment_list.remove(comment)
+                return "deleted comment"
+        
+
 class Tread(Comment):
     def __init__(self):
         Comment.__init__(self)
         self.parent_id = self.ID
+
+
+
 
 
